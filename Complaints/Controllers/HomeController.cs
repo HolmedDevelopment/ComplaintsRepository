b38653006884b655555bd167a803d90bb61a8384
HolmedMhmdT <mhamad.tabikh@holmedgroup.com>
2022-08-24 13:43:48 +0300

    commented change to test

--- Complaints/Controllers/HomeController.cs
+++ Complaints/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace Complaints.Controllers
 {
     public class HomeController : Controller
     {
+        //test to push directly to master
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
